0a561986e00687b1fbe0271da86cc0164f71b90c
Jacob Chvatal <jake@isnt.online>
2021-02-15 20:44:44 -0800

    Working flake build

--- flake.nix
+++ flake.nix
@@ -4,16 +4,14 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix/master";
 
-  outputs = { self, haskellNix, nixpkgs }: {
-    defaultPackage.x86_64-linux =
-      let pkgs = haskellNix.legacyPackages."x86_64-linux";
-      drv = pkgs.haskell-nix.project {
-        src = pkgs.haskell-nix.haskellLib.cleanGit {
-          name = "orgmode-parse";
-          src = ./.;
-        };
-        compiler-nix-name = "ghc8104";
-      };
-      in drv.components.library;
-    };
-  }
+  outputs = { self, haskellNix, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    compiler = "ghc884";
+  in
+  {
+    defaultPackage.${system} =
+      pkgs.haskell.packages.${compiler}.callPackage ./default.nix { };
+  };
+}
